b1dc36b8e4666587029c6b46d1f1c69f94cfeeb9
Tehnikumi Opilane <opilane@tptlive.ee>
2018-10-27 13:21:05 +0300

    mh

---
+++ esimeneapps/ListOfThingsActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace esimeneapps
+{
+    [Activity(Label = "ListActivity")]
+    public class ListOfThingsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ListLayout);
+
+
+
+            // Create your application here
+
+            var items = new string[] { "Tallinn", "Tartu", "Tapa" };
+
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            listView.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
+
+        }
+    }
+}
